a9aeafa2156c91dd617405a104d5a8b160f9bf70
Brett Wines <brettwines@gmail.com>
2016-06-28 22:01:10 -0400

    Begin implementing QueueStore

--- lib/Utils.hs
+++ lib/Utils.hs
@@ -2,11 +2,19 @@ module Utils
 ( senderJSONFieldModifier
 , printIO
 , putStrLnIO
+, hushT
+, note
+, hush
 ) where
 
 import qualified Data.Char as Ch
 
+import Control.Monad
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Either
+
+type Error = String
 
 camelCaseToSnakeCase :: String -> String
 camelCaseToSnakeCase [] = []
@@ -25,3 +33,17 @@ printIO = liftIO . print
 
 putStrLnIO :: (MonadIO m) => String -> m ()
 putStrLnIO = liftIO . putStrLn
+
+-- 'hush' for the respective monad transformers.
+hushT :: (Monad m) => EitherT l m r -> MaybeT m r
+hushT = MaybeT . liftM hush . runEitherT
+
+-- | Convert, with a error message to be used if the `Maybe` is `Nothing`.
+note :: Error -> Maybe b -> Either Error b
+note error Nothing = Left error
+note _ (Just x) = Right x
+
+-- | Convert, supressing the error message.
+hush :: Either l r -> Maybe r
+hush (Left _) = Nothing
+hush (Right r) = Just r
---
+++ queue-store/QueueStoreConstants.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module QueueStoreConstants
+( queueStoreDBPath
+) where
+
+import qualified Data.ByteString.Internal as BSI
+
+queueStoreDBPath :: String
+queueStoreDBPath = "queueStoreDBPath"
