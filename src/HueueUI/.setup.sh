a451cc663a98a79fb7f0c3d5d0292158dae0127d
Brett Wines <brettwines@gmail.com>
2017-03-27 01:11:58 -0700

    Update server setup script

--- src/HueueUI/.setup.sh
+++ src/HueueUI/.setup.sh
@@ -1,14 +1,14 @@
-#sudo apt-get install git;
-#git clone https://github.com/bgwines/hueue.git;
-#cd hueue;
-sudo apt-get install emacs24;
+# New server setup
+sudo apt-get -y update;
+sudo apt-get -y install g++
+sudo apt-get -y install emacs24;
+sudo apt-get -y install sqlite;
+sudo apt-get -y install libsnappy-dev;
+sudo apt-get -y install libleveldb-dev;
+sudo apt-get -y install zlib1g-dev;
 curl -sSL https://get.haskellstack.org/ | sh;
-stack setup;
-sudo apt-get install libsnappy-dev;
-wget https://leveldb.googlecode.com/files/leveldb-1.9.0.tar.gz;
-tar -xzf leveldb-1.9.0.tar.gz;
-cd leveldb-1.9.0; make;
-sudo mv libleveldb.* /usr/local/lib;
-cd include; sudo cp -R leveldb /usr/local/include; sudo ldconfig;
-cd ../..; stack build;
-sudo apt-get install sqlite
+
+git clone https://github.com/bgwines/hueue.git;
+cd hueue;
+stack setup; stack build; stack install;
+
