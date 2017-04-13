ae64cd578889c937ac01c9fb82e3842c0951b45e
Brett Wines <brettwines@gmail.com>
2017-04-12 21:17:03 -0700

    OAuthenticate

--- src/HueueUI/Main.hs
+++ src/HueueUI/Main.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main (main) where
 
@@ -13,13 +14,36 @@ import Control.Monad.Trans.Reader
 import Control.Monad.IO.Class
 import Control.Monad.Logger (runStderrLoggingT)
 
+import Control.Exception
+
+import qualified Data.Convertible as C
+
 import qualified Data.Text.Lazy as T
 
+import qualified Data.Text.Internal as TI
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSChar8
+import qualified Data.ByteString.Internal as BS
+
+import qualified Network (withSocketsDo)
+import qualified Network.CGI (formDecode)
+import qualified Network.HTTP.Conduit as HTTP
+
+import qualified Data.List as List
+
+import qualified GithubWebhook.Types.BigUser as User
+
 import qualified Job
-import qualified Repo
-import qualified Token
 import qualified JobStore
+import qualified Repo
 import qualified RepoStore
+import qualified Token
+import qualified TokenStore
+
+import qualified Utils as U
+
+import qualified Data.Aeson as A
 
 import Aliases
 import MonadImports
@@ -58,8 +82,78 @@ serve port connectionPool = scotty port $ do
             (Right js) -> return js
         json . map toJSON $ jobs
 
-    get "/:word" $ do
-        beam <- param "word"
-        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+    get "/oauthRedirect" $ do
+        accessTokenRequestCode :: String <- param "code"
+        state :: String <- param "state"
+        result <- runEitherT $ do
+            unless (state == "142857") $
+                left "Fatal: security attack detected; aborting authentication"
+
+            let githubClientID = "416fdf5ed5fb66f16bd3"
+            let githubClientSecret = "298f94844d493cc1deccf97ba54a268d1b5690a8"
+
+            let url = "http://34.208.168.142:3000"
+            let params = [ ("client_id"    , BSChar8.pack githubClientID)
+                         , ("client_secret", BSChar8.pack githubClientSecret)
+                         , ("code"         , BSChar8.pack accessTokenRequestCode)
+                         , ("redirect_uri" , BSChar8.pack $ url ++ "/receiveAccessToken")
+                         , ("state"        , "142857")
+                         ]
+
+            let githubUrl = "https://github.com/login/oauth/access_token"
+            resultBody <- HTTP.responseBody
+                <$> sendHTTPRequest githubUrl params id methodPost
+            let decodedResultBody = Network.CGI.formDecode . BSChar8.unpack . L.toStrict $ resultBody
+
+            accessToken <- hoistEither . U.note "Fatal: couldn't extract access token" $
+                snd <$> List.find ((==) "access_token" . fst) decodedResultBody
+
+            let addHeaders r = r { HTTP.requestHeaders = [("User-Agent", "hueue"), ("Authorization", BSChar8.pack ("token " ++ accessToken))]}
+            userJSON <- HTTP.responseBody <$> sendHTTPRequest "https://api.github.com/user" [] addHeaders methodGet
+            user <- (hoistEither . A.eitherDecode $ userJSON) :: EIO User.BigUser
+
+            let userID = fromIntegral $ User.id user
+            TokenStore.insert connectionPool $ Token.OAuth2Token userID accessToken
+
+        case result of
+            (Left msg) -> text $ T.pack msg
+            (Right _) -> text "success"
+
+    get "/receiveAccessToken" $ do
+        accessToken :: String <- param "access_token"
+        scope :: String <- param "scope"
+        tokenType :: String <- param "token_type"
+        liftIO . putStrLn $ "FINAL STAGE??1!!!?!!"
+        liftIO . print $ accessToken
+        liftIO . print $ scope
+        liftIO . print $ tokenType
+
 
     notFound $ text "there is no such route."
+
+type RequestModifier = HTTP.Request -> HTTP.Request
+
+methodPost :: RequestModifier
+methodPost r = r { HTTP.method = "POST" }
+
+methodGet :: RequestModifier
+methodGet r = r { HTTP.method = "GET" }
+
+sendHTTPRequest
+    :: String
+    -> [(BS.ByteString, BS.ByteString)]
+    -> RequestModifier
+    -> RequestModifier
+    -> EIO (HTTP.Response L.ByteString)
+sendHTTPRequest url params addHeaders specifyMethod
+    = bimapEitherT displayException' id
+    . EitherT
+    . liftIO
+    . try
+    . Network.withSocketsDo
+    $ do
+        req <- (addHeaders . specifyMethod . HTTP.urlEncodedBody params) <$> HTTP.parseUrl url
+        HTTP.newManager HTTP.tlsManagerSettings >>= HTTP.httpLbs req
+    where
+        displayException' :: SomeException -> String
+        displayException' = displayException
---
+++ src/TokenStore.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module TokenStore
+( loadByUserID
+, TokenStore.insert
+) where
+
+import qualified Data.Default as Default
+import qualified Data.Serialize as Serialize
+
+import MonadImports
+
+import Aliases
+
+import Data.Maybe
+
+import qualified Token
+
+import qualified Utils as U
+
+import qualified Data.Text as T
+
+import Database.Persist
+import Database.Persist.TH
+import Database.Persist.Sqlite
+import Control.Monad.Logger (runStderrLoggingT)
+
+loadByUserID :: ConnectionPool -> Int -> EIO (Maybe Token.OAuth2Token)
+loadByUserID connectionPool githubUserID = liftIO . runStderrLoggingT $ do
+    let action = getBy $ Token.UniqueUserID githubUserID
+    runSqlPool (fmap (\(Entity k t) -> t) <$> action) connectionPool
+
+insert :: ConnectionPool -> Token.OAuth2Token -> EIO ()
+insert connectionPool token = liftIO . runStderrLoggingT . void $
+    runSqlPool (Database.Persist.Sqlite.insert token) connectionPool
